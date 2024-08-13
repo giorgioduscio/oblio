4c4d766ff95b7e9462a18f4bd17d964b220d83c6
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-08-13 18:45:09 +0200

    responsive layout

--- src/app/pages/card/card.component.ts
+++ src/app/pages/card/card.component.ts
@@ -2,11 +2,12 @@ import { Component, signal, WritableSignal } from '@angular/core';
 import { CharactersService } from '../../services/characters.service';
 import { Character } from '../../services/character';
 import { GeneralitaComponent } from "./generalita/generalita.component";
+import { CompetenzeComponent } from "./competenze/competenze.component";
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [GeneralitaComponent],
+  imports: [GeneralitaComponent, CompetenzeComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
@@ -15,7 +16,7 @@ export class CardComponent {
   keys! :string[]
   constructor(private charactersService:CharactersService){
     this.character =signal(this.charactersService.characters[0])
-    this.keys =Object.keys(this.character()) .filter((key, i)=> i>0)
+    this.keys =Object.keys(this.character()) .filter((key, i)=> i>1)
     
     console.log("card", this.character());
   }
---
+++ src/app/services/privileges.ts
@@ -0,0 +1,5 @@
+export interface Privileges{
+    title:string,
+    description:string,
+    cost:number,
+}
